e533458ebb0d86005325017a89dbe13d11d3962b
Rohan Kalgutkar <hello@rohankalgutkar.in>
2018-09-23 10:06:31 +0530

    new

--- app.js
+++ app.js
@@ -1,42 +1,58 @@
 const express = require('express');
 const hbs = require('hbs');
 const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 3000;
 
 var app = express();
 
-// view engine setup
+// Express setup
 hbs.registerPartials(__dirname + '/views/partials/');
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 
-app.get('/', (req, res) => res.render('index'));
-app.get('/products', (req, res) => res.render('products'));
+// Routes
+app.get('/', (req, res) => res.render('index', {
+  pageHome: true
+}));
+app.get('/products', (req, res) => res.render('products', {
+  title: 'Products',
+  pageProduct: true
+}));
 
-var transporter = nodemailer.createTransport({
- service: 'gmail',
- auth: {
-        user: 'thetstoryco@gmail.com',
-        pass: 'DSRterrariums'
-    }
-});
+// var transporter = nodemailer.createTransport({
+//  service: 'gmail',
+//  auth: {
+//         user: 'thetstoryco@gmail.com',
+//         pass: 'DSRterrariums'
+//     }
+// });
+//
+// app.get('/contact', (req, res) => {
+//   const mailOptions = {
+//   from: 'contact@thetstory.co', // sender address
+//   to: 'rohankalgutkar@gmail.com', // list of receivers
+//   subject: 'Test mail', // Subject line
+//   html: '<p>Your html here</p>'// plain text body
+// };
+//
+//   transporter.sendMail(mailOptions, function (err, info) {
+//    if(err)
+//      console.log(err)
+//    else
+//      console.log(info);
+// });
+// });
 
-app.get('/contact', (req, res) => {
-  const mailOptions = {
-  from: 'contact@thetstory.co', // sender address
-  to: 'rohankalgutkar@gmail.com', // list of receivers
-  subject: 'Test mail', // Subject line
-  html: '<p>Your html here</p>'// plain text body
-};
-
-  transporter.sendMail(mailOptions, function (err, info) {
-   if(err)
-     console.log(err)
-   else
-     console.log(info);
-});
+app.post('/contact', (req, res) => {
+  console.log(req);
+  res.redirect('/#section-contact');
 });
 
 app.get('/full', (req, res) => res.render('index-full'));
